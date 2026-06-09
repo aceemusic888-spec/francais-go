b1095ed1a9e231fb65d63eb9c7434144f18154f1
aceemusic888-spec <aceemusic.888@gmail.com>
2026-06-09 02:05:49 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,21 @@
 const CACHE = 'francais-facile-v1';
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/francais-facile/'])));
+
+self.addEventListener('install', () => self.skipWaiting());
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
 });
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.open(CACHE).then(cache =>
+      cache.match(e.request).then(cached => {
+        const network = fetch(e.request).then(res => {
+          cache.put(e.request, res.clone());
+          return res;
+        });
+        return cached || network;
+      })
+    )
+  );
 });
